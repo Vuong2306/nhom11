09ff423365b68b7845d2543c3fe118da4a1c1c1f
Vuong <quocvuong2003gl@gmail.com>
2024-05-05 18:00:56 +0700

    Modified file 1

--- childfashion/urls.py
+++ childfashion/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views  # call to url_shortener/views.py
 
 urlpatterns = [
-    path('home', views.index, name='index')
+    path('', views.index, name='index')
 ]
