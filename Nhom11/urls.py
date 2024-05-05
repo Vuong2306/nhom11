4d5f9dee1aefa619bed282816893ebd590fa6cf4
KhangSPR <129411252+KhangSPR@users.noreply.github.com>
2024-05-05 16:57:36 +0700

    coomit

--- Nhom11/urls.py
+++ Nhom11/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+# from django.views.generic import TemplateView
 
 urlpatterns = [
+    # path('', TemplateView.as_view(template_name="home.html")),
     path('admin/', admin.site.urls),
-    path('childfashion/', include('childfashion.urls')),
+    path('', include('Home.urls')),
+    path('about/', include('About.urls')),
+    # path('childfashion/', include('childfashion.urls')),    
 ]
--- childfashion/urls.py
+++ childfashion/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from . import views  # call to url_shortener/views.py
+from django.contrib.auth import views as auth_views  # import auth_views from django.contrib.auth
+from . import views   # call to url_shortener/views.py
+# from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('home', views.index, name='index')
+    path('Home', views.index, name='index'),
+    # path('dstheloai', views.list, name='dstheloai'),
+    # path('themloai', views.themloai, name='themloai'),
+    # path('BangSP', views.list2, name='bangsp'),
+    # path('ThemSP', views.ThemSP, name='themsp'),
 ]
